fa875dec5dbb3ced7697698227c51ff5ae5ec100
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-08-02 12:16:41 +0200

    working POC

--- src/SimpleSVMs.jl
+++ src/SimpleSVMs.jl
@@ -1,5 +1,54 @@
 module SimpleSVMs
 
-greet() = print("Hello World!")
+import StatsBase
+using JuMP
+using LinearAlgebra: ⋅
+
+struct SVM{W, B}
+    w::W
+    b::B
+    SVM(w::W, b::B) where {W, B} = new{W,B}(w,b)
+end
+
+function StatsBase.fit(::Type{<:SVM}, penalty, X, y, optimizer)
+    (m, w, b) = build_svm(penalty, X, y, optimizer)
+    optimize!(m)
+    @assert termination_status(m) == JuMP.MOI.OPTIMAL
+    return SVM(JuMP.value.(w), JuP.value.(b))
+end
+
+function build_svm(penalty, X, y, optimizer)
+    (nobs, nfeat) = size(X)
+    length(y) == nobs || throw(DimensionMismatch("X and y need the same number of observations"))
+    m = JuMP.Model(optimizer)
+    @variable(m, w[1:nfeat])
+    @variable(m, b)
+    @variable(m, l[1:nobs] ≥ 0)
+    @constraint(m, hinge_loss[i=1:nobs], l[i] ≥ 1 - y[i] * (X[i,:]⋅w + b))
+    restrict_weights(m, penalty, w)
+    @objective(m, Min, sum(l))
+    return (m, w, b)
+end
+
+struct L1Penalty{R}
+    rhs::R
+    L1Penalty(rhs::R) where {R} = new{R}(rhs)
+end
+
+function restrict_weights(m::JuMP.AbstractModel, l1::L1Penalty, w)
+    nfeat = length(w)
+    @variable(m, wm[1:nfeat] ≥ 0)
+    @variable(m, wp[1:nfeat] ≥  0)
+    @constraint(m, sum(wm) + sum(wp) ≤ l1.rhs)
+end
+
+struct L2Penalty{R}
+    rhs::R
+    L2Penalty(rhs::R) where {R} = new{R}(rhs)
+end
+
+function restrict_weights(m::JuMP.AbstractModel, l2::L2Penalty, w)
+    @constraint(m, w ⋅ w ≤ l2.rhs)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,54 @@
 using SimpleSVMs
 using Test
+import Clp
+using JuMP
+import Random
+import Ipopt
 
-@testset "SimpleSVMs.jl" begin
-    # Write your own tests here.
+@testset "Improving accuracy with relaxed w" begin
+    Random.seed!(33)
+    X = vcat(rand(20, 2), rand(30,2) .+ [1,0.5]')
+    y = append!(ones(20), -ones(30))
+    prevloss = 500.0
+    for rhs in [0.0, 0.1, 0.5]
+        (m, w, b) = SimpleSVMs.build_svm(SimpleSVMs.L1Penalty(rhs), X, y, with_optimizer(Clp.Optimizer, LogLevel = 0))
+        optimize!(m)
+        res = JuMP.objective_value(m)
+        @test res < prevloss
+        prevloss = res
+        if res ≈ 0.0
+            break
+        end
+    end
+    @test prevloss ≈ 0
+end
+
+@testset "Non-separable domains" begin
+    Random.seed!(33)
+    X = vcat(randn(20, 2), randn(30,2) .+ [3.0,1.5]')
+    y = append!(ones(20), -ones(30))
+    prevloss = 500.0
+    for rhs in [0.0, 0.1, 0.5, 1.5, 100.0]
+        (m, w, b) = SimpleSVMs.build_svm(SimpleSVMs.L1Penalty(rhs), X, y, with_optimizer(Clp.Optimizer, LogLevel = 0))
+        optimize!(m)
+        res = JuMP.objective_value(m)
+        @test res ≤ prevloss
+        prevloss = res
+    end
+    @test prevloss > 0
+end
+
+@testset "L2-restriction" begin
+    Random.seed!(33)
+    X = vcat(randn(20, 2), randn(30,2) .+ [3.0,1.5]')
+    y = append!(ones(20), -ones(30))
+    prevloss = 500.0
+    for rhs in [0.0, 0.1, 0.5, 1.5, 100.0]
+        (m, w, b) = SimpleSVMs.build_svm(SimpleSVMs.L2Penalty(rhs), X, y, with_optimizer(Ipopt.Optimizer, print_level = 0))
+        optimize!(m)
+        res = JuMP.objective_value(m)
+        @test res ≤ prevloss
+        prevloss = res
+    end
+    @test prevloss > 0
 end
